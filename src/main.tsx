cdd4429316393f9ab87b317a139c6a762b03d873
Enzo Lagoela <lagoela.dev@gmail.com>
2025-06-29 22:32:39 -0300

    Implement routing with React Router for App and Projects components

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Projects from './Projects.tsx'
+
+const router = createBrowserRouter([
+  {path: "/", element: <App />},
+  {path: "/projects", element: <Projects />}
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
