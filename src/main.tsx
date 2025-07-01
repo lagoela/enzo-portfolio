144c73015974ac661f90a7321cb4f71e51def697
Enzo Lagoela <lagoela.dev@gmail.com>
2025-06-30 22:56:29 -0300

    Remove Projects component and update CSS imports; adjust border color variable in index.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Projects from './Projects.tsx'
 
 const router = createBrowserRouter([
   {path: "/", element: <App />},
-  {path: "/projects", element: <Projects />}
 ])
 
 createRoot(document.getElementById('root')!).render(
